189834f66dc3fd684d8ad91376f70772ff6a6686
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2022-08-01 11:38:44 +0200

    test script for last week

--- code/get_weekly_data.R
+++ code/get_weekly_data.R
@@ -5,6 +5,7 @@ source("analyze_images.R")
 #Get current week
 date_string <- date2ISOweek(Sys.Date())
 current_week <- substr(date_string,7,8)
+current_week <- "30"
 current_year <- as.integer(substr(date_string,1,4))
 
 #List of urls
