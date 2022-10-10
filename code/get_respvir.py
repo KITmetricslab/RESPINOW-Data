cc0e07b521a33ede6680ca597667870f7ff2e42c
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2022-10-10 18:20:09 +0200

    Update get_respvir.py

--- code/get_respvir.py
+++ code/get_respvir.py
@@ -29,7 +29,9 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
 
-driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
+from selenium import webdriver
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 driver.get(url)
 
