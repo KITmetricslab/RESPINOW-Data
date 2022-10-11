00b07795c2500e46fa51fe486c36905d9a1dbcb5
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2022-10-11 14:03:13 +0200

    Update get_respvir.py

--- code/get_respvir.py
+++ code/get_respvir.py
@@ -29,10 +29,7 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
 
-# driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
-from selenium import webdriver
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-
+driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get(url)
 
 password_field = driver.find_element("name", "password")
