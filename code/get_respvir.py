f9853771a749654e1c7d01a51ecbdac190dcb466
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2022-10-11 15:00:42 +0200

    Update get_respvir.py

--- code/get_respvir.py
+++ code/get_respvir.py
@@ -25,10 +25,9 @@ chrome_prefs = {
 }
 options.add_experimental_option("prefs", chrome_prefs)
 options.add_argument("--headless")
-
-#options.add_argument('--no-sandbox')
-#options.add_argument('--disable-dev-shm-usage')
-#options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
 
 driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get(url)
