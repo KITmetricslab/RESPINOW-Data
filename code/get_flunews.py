cbdb6c812047c06d35e159c42a2d56dbabdc151b
Chris <77851423+cbuelt@users.noreply.github.com>
2022-11-22 18:18:18 +0100

    Update get_flunews.py

--- code/get_flunews.py
+++ code/get_flunews.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 #Specify path to FluNews folder
 folder_path = "../data/FluNewsEurope/"
