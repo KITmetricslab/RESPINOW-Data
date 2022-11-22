3ab954fccebb91fb30315d554d1d8105f78214a8
Chris <77851423+cbuelt@users.noreply.github.com>
2022-11-22 19:11:22 +0100

    Update get_flunews.py

--- code/get_flunews.py
+++ code/get_flunews.py
@@ -35,8 +35,8 @@ chrome_prefs = {
     "download.default_directory": download_path
 }
 options.add_experimental_option("prefs", chrome_prefs)
-options.add_argument('--window-size=1920,1080')
-options.add_argument("--headless")
+#options.add_argument('--window-size=1920,1080')
+#options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
