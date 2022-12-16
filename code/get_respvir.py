1b6347396f44fc24e689b98a18d0fdfbd9c3985f
Chris <77851423+cbuelt@users.noreply.github.com>
2022-12-16 10:04:46 +0100

    Update get_respvir.py
    Fix chrome headless resolution

--- code/get_respvir.py
+++ code/get_respvir.py
@@ -38,6 +38,7 @@ chrome_prefs = {
 }
 options.add_experimental_option("prefs", chrome_prefs)
 options.add_argument("--headless")
+options.add_argument('--window-size=1920,1080')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
