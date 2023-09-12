c1447d46548464b891fe995ab3a34a9e9b51e45f
dwolffram <25206417+dwolffram@users.noreply.github.com>
2023-09-12 09:47:06 +0200

    add number of tests to nrz data

--- code/get_latest_data.py
+++ code/get_latest_data.py
@@ -7,6 +7,15 @@ source_dict = {
     'Survstat' : ['influenza', 'rsv', 'pneumococcal']
 }
 
+def combine_file_history(files):
+    df = pd.read_csv(files[0])
+    for f in files[1:]:
+        df_new = pd.read_csv(f)
+        df = pd.concat([df_new, df]).drop_duplicates(subset=['date', 'week', 'location', 'age_group'])
+
+    df = df.sort_values(['location', 'age_group', 'date'])
+    df.to_csv(f'../data/{source}/latest_data-{source}-{disease}{"-tests" if nrz_type == "AmountTested" else ""}.csv', index=False)
+
 for source in source_dict.keys():
     print(source)
     
@@ -14,20 +23,12 @@ for source in source_dict.keys():
         path = Path(f'../data/{source}/{disease}/')
         print(path)
         
-        if source == 'SARI':
-            files = sorted([f for f in path.rglob('*.csv') if 'test' not in f.name])
-        elif source == 'NRZ':
-            files = sorted([f for f in path.rglob('*.csv') if 'VirusDetections' in f.name])
-        elif source == 'Survstat':
+        if source == 'NRZ':
+            for nrz_type in ['VirusDetections', 'AmountTested']:
+                files = sorted([f for f in path.rglob('*.csv') if nrz_type in f.name])
+                combine_file_history(files)
+        else:
+            nrz_type = None
             files = sorted([f for f in path.rglob('*.csv')])
-            
-        df = pd.read_csv(files[0])
-
-        for f in files[1:]:
-            df_new = pd.read_csv(f)
-            df = pd.concat([df_new, df]).drop_duplicates(subset=['date', 'week', 'location', 'age_group'])
-        
-        df = df.sort_values(['location', 'age_group', 'date'])
-
-        df.to_csv(f'../data/{source}/latest_data-{source}-{disease}.csv', index=False)
+            combine_file_history(files)
         
