a733fc9d456e5eb4ccc7af0a8a9ae0d7d08da8eb
dwolffram <25206417+dwolffram@users.noreply.github.com>
2023-09-18 17:22:55 +0200

    add missing package

--- code/get_cvn.py
+++ code/get_cvn.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import requests
 from pyzipper import AESZipFile
 from io import BytesIO
+from epiweeks import Week
 
 USERNAME = os.environ['CVN_USER']
 PASSWORD = os.environ['CVN_PASSWORD']
