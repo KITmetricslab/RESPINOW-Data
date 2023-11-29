5ae4d353368f96de1cbc489603ffdbb0f666698d
Johannes Bracher <jbracher@outlook.de>
2023-11-29 11:36:11 +0100

    Update get_survstat_data.py

--- code/get_survstat_data.py
+++ code/get_survstat_data.py
@@ -139,12 +139,13 @@ def get_commits(disease, date, stratum):
 
 def get_sha(disease, date):
     df1 = get_commits(disease, date, 'states')
-    # df1 = df1[df1["date"].hour < 20]
+    df1 = df1[df1.date.hour < 20]
+    print(df1)
 
     
     df2 = get_commits(disease, date, 'age')
-    print(df2.date.hour)
-    # df2 = df2[df2["date"].hour < 20]
+    df2 = df2[df2.date.hour < 20]
+    print(df2)
     
     df = pd.concat([df1, df2])
     
