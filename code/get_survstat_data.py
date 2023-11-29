a9c89488178025149bd0ac7f4e0c11d3850fad3f
jbracher <jbracher@outlook.de>
2023-11-29 10:56:02 +0100

    Johannes traing to play with Python code...

--- code/get_survstat_data.py
+++ code/get_survstat_data.py
@@ -139,11 +139,15 @@ def get_commits(disease, date, stratum):
 
 def get_sha(disease, date):
     df1 = get_commits(disease, date, 'states')
+    df1 = df1[df1["date"].hour < 20]
+    
     df2 = get_commits(disease, date, 'age')
+    df2 = df2[df2["date"].hour < 20]
+    
     df = pd.concat([df1, df2])
     
     # use the later date of the first commits
-    sha = df.loc[df.date == df.groupby('stratum')['date'].min().max(), 'sha'].iloc[0]
+    sha = df.loc[df.date == df.groupby('stratum')['date'].max().max(), 'sha'].iloc[0]
     return sha
 
 
