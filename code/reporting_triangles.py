30f90701bfe6e59d0dd7b5767695e2dce76fd46f
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2024-09-26 14:22:15 +0200

    Update reporting_triangles.py

--- code/reporting_triangles.py
+++ code/reporting_triangles.py
@@ -216,7 +216,7 @@ def compute_reporting_triangle(source, disease, tests=False, max_delay=10, prosp
             f'-{disease}{"-tests" if tests else ""}.csv')
 
     if source == 'SARI':
-        df = df[(df['date'] >= pd.Timestamp('2023-10-22')) | (df['age_group'] == '00+')]
+        df = df[(df['date'] >= pd.Timestamp('2023-10-22').date()) | (df['age_group'] == '00+')]
     
     df.to_csv(path, index=False)
 
