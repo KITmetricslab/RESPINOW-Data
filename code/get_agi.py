928df89787648bb1be7e67dc0e8c7aeb6a249b0d
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2025-04-23 11:26:23 +0200

    Update get_agi.py

--- code/get_agi.py
+++ code/get_agi.py
@@ -105,6 +105,8 @@ new_dates = [date for date in tags if previous_sunday(date) not in dates_process
 for date in new_dates:
     print(date)
     df = load_file_from_tag(OWNER, REPO, FILEPATH, date)
+    print(df.head())
     df = preprocess_ARE(df)
+    print(df.head())
     df.to_csv(f'../data/AGI_abs/are/{previous_sunday(date)}-agi-are.csv', index=False)
     
